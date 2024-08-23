9adca1064de926c9ba07702715baf3cfccb8e46e
Vitória Oliveira <mvitoriacoliveira@gmail.com>
2024-08-23 14:37:13 +0200

    add: veh/rep dao's to db class

--- app/src/main/java/com/example/autogestion/data/ClientDatabase.kt
+++ app/src/main/java/com/example/autogestion/data/ClientDatabase.kt
@@ -2,11 +2,15 @@ package com.example.autogestion.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.autogestion.data.dao.ReparationDao
+import com.example.autogestion.data.dao.VehicleDao
 
 @Database(entities = [Client::class, Vehicle::class, Reparation::class], version = 1, exportSchema = false)
 abstract class ClientDatabase : RoomDatabase() {
 
     abstract fun clientDao(): ClientDao
+    abstract fun vehicleDao(): VehicleDao
+    abstract fun reparationDao(): ReparationDao
 
     companion object {
         @Volatile
